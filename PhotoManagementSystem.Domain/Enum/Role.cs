46d8ddca41924409e5790e8a5ea0c3a7f38757b3
PaulSmocot <110483174+PaulSmocot@users.noreply.github.com>
2023-04-22 13:22:16 +0300

    create-models

---
+++ PhotoManagementSystem.Domain/Enum/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoManagementSystem.Domain.Enum
+{
+    public enum Role
+    {
+        Fotograf,
+        Client,
+        Administrator
+    }
+}
---
+++ PhotoManagementSystem.Domain/Models/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoManagementSystem.Domain.Models
+{
+    public class Camera
+    {
+        public Guid Id { get; set; }
+        public string Body { get; set; }
+        public string Lens { get; set; }
+
+        public string Flash { get; set; }
+    }
+}
